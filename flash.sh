4f9359c6b1c2d22d69b4219bc6da0d27c915a75f
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-02-22 19:58:19 -0500

    Update flash.sh

--- flash.sh
+++ flash.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# hold reset button while pluggin your MCU 
-# into USB to enter bootloader mode
+# hold reset button while pluggin your Leonardo ATmega32U4 
+# into USB until 3 yellow lights flash
+# to enter bootloader mode
 
-avrdude -p m32u4 -c avr109 -P /dev/ttyACM0 -U flash:w:/path/to/your/firmware.hex 
+avrdude -p m32u4 -c avr109 -P /dev/ttyACM0 -U flash:w:./I2CUSB.hex 
+
